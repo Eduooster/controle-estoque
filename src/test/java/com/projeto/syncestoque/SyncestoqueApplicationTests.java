72062667f1a099b7bd522c0af5a07d69a452060a
Seu Nome <edudz1404@gmail.com>
2025-07-18 17:19:01 -0300

    fc

---
+++ src/test/java/com/projeto/syncestoque/SyncestoqueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.syncestoque;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SyncestoqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
